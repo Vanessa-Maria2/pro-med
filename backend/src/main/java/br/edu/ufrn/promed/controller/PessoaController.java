bd52e9ee25aca2407affcfa440700729565a761b
andremedeiros03 <medeiros.andre05@gmail.com>
2025-07-04 15:41:11 -0300

    Add login endpoint and update database configuration
    - Implemented `/login` endpoint in `PessoaController` with authentication logic.
    - Created `LoginRequestDto` for login payload processing.
    - Extended `PessoaRepository` with a `findByEmail` method.
    - Enhanced `PessoaService` for fetching user by email.
    - Updated `application.properties` with local database connection details for development.

--- backend/src/main/java/br/edu/ufrn/promed/controller/PessoaController.java
+++ backend/src/main/java/br/edu/ufrn/promed/controller/PessoaController.java
@@ -1,8 +1,11 @@
 package br.edu.ufrn.promed.controller;
 
+import br.edu.ufrn.promed.model.LoginRequestDto;
 import br.edu.ufrn.promed.model.Pessoa;
 import br.edu.ufrn.promed.model.PessoaDto;
+import br.edu.ufrn.promed.service.AuthService;
 import br.edu.ufrn.promed.service.PessoaService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,14 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class PessoaController {
 
     private final PessoaService pessoaService;
+    private final AuthService authService;
 
-    public PessoaController(PessoaService pessoaService) {
+    public PessoaController(PessoaService pessoaService, AuthService authService) {
         this.pessoaService = pessoaService;
+        this.authService = authService;
     }
 
     @PostMapping
     public ResponseEntity<Pessoa> cadastro(@RequestBody PessoaDto pessoaDto) {
         var pessoa = pessoaService.cadastro(pessoaDto);
         return ResponseEntity.ok(pessoa);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto){
+        if (loginRequestDto.getEmail() == null || loginRequestDto.getPassword() == null){
+            return ResponseEntity.badRequest().body("Email e senha são obrigatórios");
+        }
+
+        boolean isAuthenticated = authService.authenticate(loginRequestDto);
+
+        if(isAuthenticated){
+            return ResponseEntity.ok("Login bem-sucessido!");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas.");
+        }
     }
 }
--- backend/src/main/java/br/edu/ufrn/promed/repository/PessoaRepository.java
+++ backend/src/main/java/br/edu/ufrn/promed/repository/PessoaRepository.java
@@ -6,6 +6,7 @@ import br.edu.ufrn.promed.util.PasswordUtil;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
+import java.util.Optional;
 
 @Repository
 public class PessoaRepository {
@@ -38,5 +39,31 @@ public class PessoaRepository {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Optional<Pessoa> findByEmail(String email) {
+        String sql = "SELECT cpf, nome, sobrenome, email, endereco, data_nascimento, senha FROM pessoa WHERE email = ?";
+        Pessoa pessoa = null;
+        try {
+            Connection connection = databaseConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                pessoa = new Pessoa();
+                pessoa.setCpf(rs.getString("cpf"));
+                pessoa.setNome(rs.getString("nome"));
+                pessoa.setSobrenome(rs.getString("sobrenome"));
+                pessoa.setEmail(rs.getString("email"));
+                pessoa.setEndereco(rs.getString("endereco"));
+                pessoa.setDataNascimento(rs.getDate("data_nascimento"));
+                pessoa.setSenha(rs.getString("senha"));
+            }
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.ofNullable(pessoa);
     }
 }
